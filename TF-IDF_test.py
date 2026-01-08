fbaaa3d9214250759ccd2bbc217ec0fd32b4e008
Knudsenmarlin <Knudsenmarlin@gmail.com>
2026-01-08 16:16:56 +0100

    Crazy updates :3

--- TF-IDF_test.py
+++ TF-IDF_test.py
@@ -1,12 +1,14 @@
 import re
 import nltk
-import os
-from pathlib import Path
 from nltk.tokenize import word_tokenize
 nltk.download('punkt_tab', quiet=True)
 stemmer = nltk.stem.PorterStemmer()
+import os
+from pathlib import Path
+import math
 
-AIWords = 'a the delve underscore showcase unveil intricate meticulous pivotal heighten nuance bolster foster interplay garnered excels aligning notably additionally significant potential findings crucial exhibit comprehensive notably enhance valuable'
+
+AIWords = 'delve underscore showcase unveil intricate meticulous pivotal heighten nuance bolster foster interplay garnered excels aligning notably additionally significant potential findings crucial exhibit comprehensive notably enhance valuable'
 AIWordsStemmed = [stemmer.stem(word) for word in word_tokenize(AIWords)]
 
 def txtToWords(filename):
@@ -28,27 +30,73 @@ def countWords(stemmedText):
             occurancesTerm[word] = occurancesTerm.get(word) + count
     return occurancesTerm, totalWords
 
-def totalDocuments(folder):
-    folder_path = Path(folder)
-    num_files = sum(1 for f in folder_path.iterdir() if f.is_file())
+def countTotalDocuments(*folder):
+    num_files = 0
+    for folderName in folder:
+        folder_path = Path(folderName)
+        num_files += sum(1 for f in folder_path.iterdir() if f.is_file())
     return num_files
 
-def countDocumentsWithTerm(folder):
-    folder_path = Path(folder)
-    documentTerms = dict()
-    for f in folder_path.glob('*.txt'):
-        stemmedText = txtToWords(f)
-        for word in AIWordsStemmed:
-            if word in stemmedText:
-                if documentTerms.get(word) is None:
-                    documentTerms[word] = 1
-                else:
-                    documentTerms[word] = documentTerms.get(word) + 1
-    return documentTerms
+def countDocumentsWithTerm(*folder):
+    documentTermsFinalDict = dict()
+    for folderName in folder:
+        folder_path = Path(folderName)
+        documentTerms = dict()
+        for f in folder_path.glob('*.txt'):
+            stemmedText = txtToWords(f)
+            for word in AIWordsStemmed:
+                if word in stemmedText:
+                    if documentTerms.get(word) is None:
+                        documentTerms[word] = 1
+                    else:
+                        documentTerms[word] = documentTerms.get(word) + 1
+        documentTermsFinalDict = combineDictionaries(documentTermsFinalDict, documentTerms)
+    return documentTermsFinalDict
+
+def combineDictionaries(*dicts):
+    finalDict = dict()
+    for tempDict in dicts:
+        for key in tempDict:
+            if finalDict.get(key) is None:
+                finalDict[key] = tempDict.get(key)
+            else:
+                finalDict[key] = finalDict.get(key) + tempDict.get(key)
+    return finalDict
+
+def findCombinedTFIDFScore(document):
+    TFIDFDict = dict()
+    occurancesTerm, totalWordsInDocument = countWords(txtToWords(document))
+    documentsWithTerm = countDocumentsWithTerm('HumanArticles')
+    totalDocuments = countTotalDocuments('HumanArticles')
+    for word in AIWordsStemmed:
+        termFrequency = occurancesTerm[word] / totalWordsInDocument
+        if documentsWithTerm.get(word) is None:
+            inverseDocumentFrequency = 0
+        else:
+            inverseDocumentFrequency = math.log(totalDocuments / documentsWithTerm[word])
+        TFIDF = termFrequency * inverseDocumentFrequency
+        TFIDFDict[word] = TFIDF
+    combinedTFIDF = 0
+    for key in TFIDFDict:
+        combinedTFIDF += TFIDFDict[key]
+    return combinedTFIDF
+    
 
 
-testStemmedText = txtToWords('HumanArticles\s_Article1H.txt')
-print(countWords(testStemmedText))
-print(totalDocuments('HumanArticles'))
-print(countDocumentsWithTerm('HumanArticles'))
-print(countDocumentsWithTerm('AIArticles'))
+
+if __name__ == "__main__":
+    print(findCombinedTFIDFScore('HumanArticles\l_Article2H.txt'))
+    print(findCombinedTFIDFScore('HumanArticles\l_Article3H.txt'))
+    print(findCombinedTFIDFScore('HumanArticles\l_Article4H.txt'))
+    print(findCombinedTFIDFScore('HumanArticles\l_Article5H.txt'))
+    print(findCombinedTFIDFScore('HumanArticles\l_Article6H.txt'))
+    print(findCombinedTFIDFScore('HumanArticles\l_Article7H.txt'))
+    print(findCombinedTFIDFScore('DeepSeekArticles\l_Article1DS.txt'))
+    print(findCombinedTFIDFScore('DeepSeekArticles\l_Article2DS.txt'))
+    print(findCombinedTFIDFScore('DeepSeekArticles\l_Article3DS.txt'))
+    print(findCombinedTFIDFScore('ChatGPTArticles\l_Article1GPT.txt'))
+    print(findCombinedTFIDFScore('ChatGPTArticles\l_Article2GPT.txt'))
+    print(findCombinedTFIDFScore('ChatGPTArticles\l_Article3GPT.txt'))
+    print(findCombinedTFIDFScore('ChatGPTArticles\l_Article4GPT.txt'))
+
+
