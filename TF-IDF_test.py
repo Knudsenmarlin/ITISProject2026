b8a95885298a4abe69739c5bd96fed07d7ed1add
Knudsenmarlin <Knudsenmarlin@gmail.com>
2026-01-08 18:33:27 +0100

    added an average function

--- TF-IDF_test.py
+++ TF-IDF_test.py
@@ -6,12 +6,15 @@ stemmer = nltk.stem.PorterStemmer()
 import os
 from pathlib import Path
 import math
+import numpy as np
 
-
-AIWords = 'delve underscore showcase unveil intricate meticulous pivotal heighten nuance bolster foster interplay garnered excels aligning notably additionally significant potential findings crucial exhibit comprehensive notably enhance valuable'
+AIWordsList = ['delve', 'underscore', 'showcase', 'unveil', 'intricate', 'meticulous', 'pivotal', 'heighten',
+               'nuance', 'bolster', 'foster', 'interplay', 'garnered', 'excels', 'aligning', 'notably', 'additionally', 
+               'significant', 'potential', 'crucial', 'exhibit', 'comprehensive', 'notably', 'valuable', 'enhance',]
+AIWords = ' '.join(AIWordsList)
 AIWordsStemmed = [stemmer.stem(word) for word in word_tokenize(AIWords)]
 
-def txtToWords(filename):
+def txt_to_words(filename):
     file = open(filename)
     file = file.read().replace('\n', ' ')
     clean_text = re.sub(r'[^a-zA-Z ]', '', file)
@@ -19,7 +22,7 @@ def txtToWords(filename):
     stemmedText = [stemmer.stem(word) for word in word_tokenize(clean_text)]
     return stemmedText
 
-def countWords(stemmedText):
+def count_words(stemmedText):
     occurancesTerm = dict()
     totalWords = len(stemmedText)
     for word in AIWordsStemmed:
@@ -30,30 +33,30 @@ def countWords(stemmedText):
             occurancesTerm[word] = occurancesTerm.get(word) + count
     return occurancesTerm, totalWords
 
-def countTotalDocuments(*folder):
+def count_total_documents(*folder):
     num_files = 0
     for folderName in folder:
         folder_path = Path(folderName)
         num_files += sum(1 for f in folder_path.iterdir() if f.is_file())
     return num_files
 
-def countDocumentsWithTerm(*folder):
+def count_documents_with_term(*folder):
     documentTermsFinalDict = dict()
     for folderName in folder:
         folder_path = Path(folderName)
         documentTerms = dict()
         for f in folder_path.glob('*.txt'):
-            stemmedText = txtToWords(f)
+            stemmedText = txt_to_words(f)
             for word in AIWordsStemmed:
                 if word in stemmedText:
                     if documentTerms.get(word) is None:
                         documentTerms[word] = 1
                     else:
                         documentTerms[word] = documentTerms.get(word) + 1
-        documentTermsFinalDict = combineDictionaries(documentTermsFinalDict, documentTerms)
+        documentTermsFinalDict = combine_dictionaries(documentTermsFinalDict, documentTerms)
     return documentTermsFinalDict
 
-def combineDictionaries(*dicts):
+def combine_dictionaries(*dicts):
     finalDict = dict()
     for tempDict in dicts:
         for key in tempDict:
@@ -63,11 +66,11 @@ def combineDictionaries(*dicts):
                 finalDict[key] = finalDict.get(key) + tempDict.get(key)
     return finalDict
 
-def findCombinedTFIDFScore(document):
+def find_combined_TFIDF_score(document):
     TFIDFDict = dict()
-    occurancesTerm, totalWordsInDocument = countWords(txtToWords(document))
-    documentsWithTerm = countDocumentsWithTerm('HumanArticles')
-    totalDocuments = countTotalDocuments('HumanArticles')
+    occurancesTerm, totalWordsInDocument = count_words(txt_to_words(document))
+    documentsWithTerm = count_documents_with_term('HumanArticles', 'ChatGPTArticles', 'DeepSeekArticles')
+    totalDocuments = count_total_documents('HumanArticles', 'ChatGPTArticles', 'DeepSeekArticles')
     for word in AIWordsStemmed:
         termFrequency = occurancesTerm[word] / totalWordsInDocument
         if documentsWithTerm.get(word) is None:
@@ -79,24 +82,23 @@ def findCombinedTFIDFScore(document):
     combinedTFIDF = 0
     for key in TFIDFDict:
         combinedTFIDF += TFIDFDict[key]
+    print(combinedTFIDF)
     return combinedTFIDF
     
-
+def average_TDFIF_score(*folder):
+    scoreList = np.array([])
+    for folderName in folder:
+        folder_path = Path(folderName)
+        for f in folder_path.iterdir():
+            if f.is_file():
+                scoreList = np.append(scoreList, find_combined_TFIDF_score(f))
+    scoreMean = np.mean(scoreList)
+    scoreStd = np.std(scoreList, ddof=1)
+    return scoreMean, scoreStd
 
 
 if __name__ == "__main__":
-    print(findCombinedTFIDFScore('HumanArticles\l_Article2H.txt'))
-    print(findCombinedTFIDFScore('HumanArticles\l_Article3H.txt'))
-    print(findCombinedTFIDFScore('HumanArticles\l_Article4H.txt'))
-    print(findCombinedTFIDFScore('HumanArticles\l_Article5H.txt'))
-    print(findCombinedTFIDFScore('HumanArticles\l_Article6H.txt'))
-    print(findCombinedTFIDFScore('HumanArticles\l_Article7H.txt'))
-    print(findCombinedTFIDFScore('DeepSeekArticles\l_Article1DS.txt'))
-    print(findCombinedTFIDFScore('DeepSeekArticles\l_Article2DS.txt'))
-    print(findCombinedTFIDFScore('DeepSeekArticles\l_Article3DS.txt'))
-    print(findCombinedTFIDFScore('ChatGPTArticles\l_Article1GPT.txt'))
-    print(findCombinedTFIDFScore('ChatGPTArticles\l_Article2GPT.txt'))
-    print(findCombinedTFIDFScore('ChatGPTArticles\l_Article3GPT.txt'))
-    print(findCombinedTFIDFScore('ChatGPTArticles\l_Article4GPT.txt'))
-
-
+    print(find_combined_TFIDF_score('HumanArticles\l_Article2H.txt'))
+    print(average_TDFIF_score('HumanArticles'))
+    print(average_TDFIF_score('ChatGPTArticles'))
+    print(average_TDFIF_score('DeepSeekArticles'))
